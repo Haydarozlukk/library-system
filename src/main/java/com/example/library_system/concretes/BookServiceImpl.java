efd4b008350f89cb68ea215c7f79570eaefa17c2
haydarozlukk <haydarozlukk@gmail.com>
2024-08-10 00:40:39 +0300

    First Commit

---
+++ src/main/java/com/example/library_system/concretes/BookServiceImpl.java
@@ -0,0 +1,47 @@
+package com.example.librarysystem.concretes;
+
+import com.example.librarysystem.abstracts.BookService;
+import com.example.librarysystem.model.Book;
+import com.example.librarysystem.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class BookServiceImpl implements BookService {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Override
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+
+    @Override
+    public Optional<Book> getBookById(Long id) {
+        return bookRepository.findById(id);
+    }
+
+    @Override
+    public Book addBook(Book book) {
+        return bookRepository.save(book);
+    }
+
+    @Override
+    public Book updateBook(Long id, Book bookDetails) {
+        Book book = bookRepository.findById(id).orElseThrow(() -> new RuntimeException("Book not found"));
+        book.setTitle(bookDetails.getTitle());
+        book.setAuthor(bookDetails.getAuthor());
+        book.setIsbn(bookDetails.getIsbn());
+        book.setPublicationYear(bookDetails.getPublicationYear());
+        return bookRepository.save(book);
+    }
+
+    @Override
+    public void deleteBook(Long id) {
+        bookRepository.deleteById(id);
+    }
+}
